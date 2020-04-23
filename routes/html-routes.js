d59db3bf562503d188b038e873bbdaccd1a3197a
Stephanie smith <smithse4@gmail.com>
2020-04-23 13:13:16 -0400

    updated html routes to dist folder

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -20,27 +20,27 @@ module.exports = function (app) {
     res.redirect("/library");
   });
   app.get("/explore", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/explore.html"));
+    res.sendFile(path.join(__dirname, "../dist/explore.html"));
   });
   app.get("/reviews", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/reviews.html"));
+    res.sendFile(path.join(__dirname, "../dist/reviews.html"));
   });
   app.get("/login", function (req, res) {
     if (req.user) {
       res.redirect("/");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../dist/login.html"));
   });
   app.get("/signup", function (req, res) {
     if (req.user) {
       res.redirect("/");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../dist/signup.html"));
   });
   app.get("/explore", isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/explore.html"));
+    res.sendFile(path.join(__dirname, "../dist/explore.html"));
   });
   app.get("/reviews", isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/reviews.html"));
+    res.sendFile(path.join(__dirname, "../dist/reviews.html"));
   });
 };
